98a5046c3119ce9736df0eb52ec3bd9720be97f8
Stephen E Ingram <singram@openwhere.com>
2017-11-16 20:06:59 -0500

    Add output logic and intercept code

--- readlayer.cpp
+++ readlayer.cpp
@@ -2,6 +2,8 @@
 // Created by Stephen E Ingram on 11/9/17.
 //
 
+#include <typeinfo>
+#include <iostream>
 #include "readlayer.h"
 #include <mapbox/geometry/multi_point.hpp>
 #include <mapbox/geometry/multi_line_string.hpp>
@@ -61,6 +63,100 @@ int test_op_within(oppoint op, long long extent) {
     return return_flags;
 }
 
+bool intercept_segments(long long x1, long long y1, long long x2, long long y2, long long x3, long long y3, long long x4, long long y4, double &x_intercept, double &y_intercept) {
+    double denominator = (double)(((x1 - x2)*(y3 - y4)) - ((y1 - y2)*(x3 - x4)));
+    if (denominator == 0.0) {
+        return false;
+    }
+
+    double x = (double)(((x2*y2 - y1*x2) * (x3 - x4)) - ((x1 - x2) * (x3*y4 - y3*x4))) / denominator;
+    double y = (double)(((x1*y2 - y1*x2) * (y3 - y4)) - ((y1 - y2) * (x3*y4 - y3*x4))) / denominator;
+    x_intercept = x;
+    y_intercept = y;
+    return true;
+}
+
+int edge_intercept(int edge, point_type p1, point_type p2, long long extent, point_type &intercept) {
+    bool vertical = p1.x == p2.x;
+    double x_intercept = 0.0;
+    double y_intercept = 0.0;
+    switch (edge) {
+        case EAST:
+            if (vertical) {
+                if (p1.x == extent - 1) {
+                    return COINCIDENT;
+                }
+                return NO_INTERCECT;
+            } else {
+                if ( intercept_segments(0, 0, 0, extent-1, p1.x, p1.y, p2.x, p2.y, x_intercept, y_intercept) ) {
+                    if ( x_intercept != double(0) ) {
+                        return NO_INTERCECT;
+                    } else if ( y_intercept < 0 || y_intercept >= double(extent) ) {
+                        return NO_INTERCECT;
+                    }
+                    intercept.x = floor(x_intercept + 0.5);
+                    intercept.y = floor(y_intercept + 0.5);
+                    return INTERCECT;
+                } else {
+                    return COINCIDENT;
+                }
+            }
+        case WEST:
+            if (vertical) {
+                if (p1.x == 0) {
+                    return COINCIDENT;
+                }
+                return NO_INTERCECT;
+            } else {
+                if ( intercept_segments(extent-1, 0, extent-1, extent-1, p1.x, p1.y, p2.x, p2.y, x_intercept, y_intercept) ) {
+                    if ( x_intercept != double(extent-1) ) {
+                        return NO_INTERCECT;
+                    } else if ( y_intercept < 0.0 || y_intercept >= double(extent) ) {
+                        return NO_INTERCECT;
+                    }
+                    intercept.x = floor(x_intercept + 0.5);
+                    intercept.y = floor(y_intercept + 0.5);
+                    return INTERCECT;
+                } else {
+                    return COINCIDENT;
+                }
+            }
+        case NORTH:
+            if ( vertical ) {
+                x_intercept = p1.x;
+                y_intercept = 0.0;
+            } else if ( intercept_segments(0, 0, extent-1, 0, p1.x, p1.y, p2.x, p2.y, x_intercept, y_intercept) ) {
+                if ( y_intercept != double(0) ) {
+                    return NO_INTERCECT;
+                } else if ( x_intercept < 0.0 || x_intercept >= double(extent) ) {
+                    return NO_INTERCECT;
+                }
+                intercept.x = floor(x_intercept + 0.5);
+                intercept.y = floor(y_intercept + 0.5);
+                return INTERCECT;
+            } else {
+                return COINCIDENT;
+            }
+        case SOUTH:
+            if ( vertical ) {
+                x_intercept = p1.x;
+                y_intercept = extent-1;
+            } else if ( intercept_segments(0, extent-1, extent-1, extent-1, p1.x, p1.y, p2.x, p2.y, x_intercept, y_intercept) ) {
+                if ( y_intercept != double(extent-1) ) {
+                    return NO_INTERCECT;
+                } else if ( x_intercept < 0.0 || x_intercept >= double(extent) ) {
+                    return NO_INTERCECT;
+                }
+                intercept.x = floor(x_intercept + 0.5);
+                intercept.y = floor(y_intercept + 0.5);
+                return INTERCECT;
+            } else {
+                return COINCIDENT;
+            }
+    }
+    return NO_INTERCECT;
+}
+
 geometry_type extract_geometry(mvt_feature const &feat, long long extent, int &within) {
     std::vector <oppoint> ops;
     for (size_t g = 0; g < feat.geometry.size(); g++) {
@@ -77,11 +173,10 @@ geometry_type extract_geometry(mvt_feature const &feat, long long extent, int &w
 
     if (feat.type == VT_POINT) {
         if (ops.size() == 1) {
-            within = test_op_within(ops[0], extent) == 0;
+            within = test_op_within(ops[0], extent);
             return point_type(ops[0].x, ops[0].y);
         } else {
             multi_point_type pm;
-            within = true;
             for (size_t i = 0; i < ops.size(); i++) {
                 within |= test_op_within(ops[i], extent);
                 pm.push_back(point_type(ops[i].x, ops[i].y));
@@ -98,7 +193,6 @@ geometry_type extract_geometry(mvt_feature const &feat, long long extent, int &w
 
         if (movetos < 2) {
             line_type line;
-            within = true;
             for (size_t i = 0; i < ops.size(); i++) {
                 within |= test_op_within(ops[i], extent);
                 line.push_back(point_type(ops[i].x, ops[i].y));
@@ -108,27 +202,26 @@ geometry_type extract_geometry(mvt_feature const &feat, long long extent, int &w
             multi_line_type mline;
             int state = 0;
 
-            line_type *lt = new line_type();
-            mline.push_back(*lt);
-            within = true;
+            line_type lt;
             for (size_t i = 0; i < ops.size(); i++) {
                 if (ops[i].op == VT_MOVETO) {
                     if (state == 0) {
                         within |= test_op_within(ops[i], extent);
-                        lt->push_back(point_type(ops[i].x, ops[i].y));
+                        lt.push_back(point_type(ops[i].x, ops[i].y));
                         state = 1;
                     } else {
-                        lt = new line_type();
-                        mline.push_back(*lt);
+                        mline.push_back(lt);
+                        lt.clear();
                         within |= test_op_within(ops[i], extent);
-                        lt->push_back(point_type(ops[i].x, ops[i].y));
+                        lt.push_back(point_type(ops[i].x, ops[i].y));
                         state = 1;
                     }
                 } else {
                     within |= test_op_within(ops[i], extent);
-                    lt->push_back(point_type(ops[i].x, ops[i].y));
+                    lt.push_back(point_type(ops[i].x, ops[i].y));
                 }
             }
+            mline.push_back(lt);
             return mline;
         }
     } else if (feat.type == VT_POLYGON) {
@@ -140,8 +233,6 @@ geometry_type extract_geometry(mvt_feature const &feat, long long extent, int &w
                 rings.push_back(std::vector<oppoint>());
                 areas.push_back(0);
             }
-
-            within = true;
 
             int n = rings.size() - 1;
             if (n >= 0) {
@@ -179,15 +270,11 @@ geometry_type extract_geometry(mvt_feature const &feat, long long extent, int &w
             }
         }
 
-        polygon_type *poly = new polygon_type();
-        linear_ring_type *lr = new linear_ring_type();
-        poly->push_back(*lr);
+        polygon_type poly;
+        linear_ring_type lr;
 
-        multi_polygon_type *mpoly = new multi_polygon_type();
+        multi_polygon_type mpoly;
 
-        if (outer > 1) {
-            mpoly->push_back(*poly);
-        }
         int state = 0;
         for (size_t i = 0; i < rings.size(); i++) {
             if (i == 0 && areas[i] < 0) {
@@ -198,35 +285,38 @@ geometry_type extract_geometry(mvt_feature const &feat, long long extent, int &w
             if (areas[i] >= 0) {
                 if (state != 0) {
                     // new multipolygon
-                    poly = new polygon_type();
-                    lr = new linear_ring_type();
-                    poly->push_back(*lr);
-                    mpoly->push_back(*poly);
+                    poly.push_back(lr);
+                    mpoly.push_back(poly);
+                    lr.clear();
+                    poly.clear();
                 }
                 state = 1;
             }
 
             if (state == 2) {
                 // new ring in the same polygon
-                lr = new linear_ring_type();
-                poly->push_back(*lr);
+                poly.push_back(lr);
+                lr.clear();
             }
 
             for (size_t j = 0; j < rings[i].size(); j++) {
                 if (rings[i][j].op != VT_CLOSEPATH) {
-                    lr->push_back(point_type(rings[i][j].x, rings[i][j].y));
+                    lr.push_back(point_type(rings[i][j].x, rings[i][j].y));
                 } else {
-                    lr->push_back(point_type(rings[i][0].x, rings[i][0].y));
+                    lr.push_back(point_type(rings[i][0].x, rings[i][0].y));
                 }
             }
             state = 2;
         }
         if (outer > 1) {
-            return *mpoly;
+            poly.push_back(lr);
+            mpoly.push_back(poly);
+            return mpoly;
         }
-        return *poly;
+        poly.push_back(lr);
+        return poly;
     }
-    within = true;
+    within = 0x0;
     return point_type(0, 0);
 }
 
@@ -287,4 +377,195 @@ std::vector<feature_type> layer_to_features(mvt_layer const &layer, unsigned z, 
         features.push_back(current_feature);
     }
     return features;
+}
+
+struct property_writer {
+    property_writer() {}
+
+    void operator()(std::string const & s) const {
+        fprintf(stdout, "\"%s\"", s.c_str());
+    }
+
+    void operator()(uint64_t const & u) const {
+        fprintf(stdout, "%llu", u);
+    }
+
+    void operator()(int64_t const & i) const {
+        fprintf(stdout, "%lld", i);
+    }
+
+    void operator()(bool const & b) const {
+        fprintf(stdout, "%s", b ? "true" : "false");
+    }
+
+    void operator()(double const & d) const {
+        fprintf(stdout, "%f", d);
+    }
+
+    template <typename T>
+    void operator()(T const & g) const {
+        std::clog << "encountered unhandled value";
+    }
+};
+
+struct shape_writer {
+    shape_writer() {}
+
+    void operator()(point_type const & pt) const {
+        fprintf(stdout, "{ \"type\": \"Point\", \"coordinates\": [ %lld, %lld ] }", pt.x, pt.y);
+    }
+
+    void operator()(multi_point_type  & pts) const {
+        fprintf(stdout, "{ \"type\": \"MultiPoint\", \"coordinates\": [");
+        bool comma = false;
+        for (auto pt = pts.begin(); pt != pts.end(); ++pt) {
+             if ( comma ) {
+                 fprintf(stdout, ",");
+             } else {
+                 comma = true;
+             }
+            fprintf(stdout, " [ %lld, %lld ]", pt->x, pt->y);
+        }
+        fprintf(stdout, " ] }");
+    }
+
+
+    void operator()(line_type const & ln) const {
+        fprintf(stdout, "{ \"type\": \"LineString\", \"coordinates\": [");
+        bool comma = false;
+        for ( auto pt = ln.begin(); pt != ln.end(); ++pt) {
+            if ( comma ) {
+                fprintf(stdout, ",");
+            } else {
+                comma = true;
+            }
+            fprintf(stdout, " [ %lld, %lld ]", pt->x, pt->y);
+        }
+        fprintf(stdout, " ] }");
+    }
+
+    void operator()(multi_line_type const & lns) const {
+        fprintf(stdout, "{ \"type\": \"MultiLineString\", \"coordinates\": [");
+        bool line_comma = false;
+
+        for ( auto it = lns.begin(); it != lns.end(); ++it ) {
+            if ( line_comma ) {
+                fprintf(stdout, ",");
+            } else {
+                line_comma = true;
+            }
+
+            bool comma = false;
+            fprintf(stdout, " [");
+            for ( auto pt = it->begin(); pt != it->end(); ++pt) {
+                if ( comma ) {
+                    fprintf(stdout, ",");
+                } else {
+                    comma = true;
+                }
+                fprintf(stdout, " [ %lld, %lld ]", pt->x, pt->y);
+            }
+            fprintf(stdout, " ]");
+        }
+        fprintf(stdout, " ] }");
+    }
+
+    void operator()(polygon_type const & poly) const {
+        fprintf(stdout, "{ \"type\": \"Polygon\", \"coordinates\": [");
+        bool ring_comma = false;
+        for ( auto ring = poly.begin(); ring != poly.end(); ++ring) {
+            if ( ring_comma ) {
+                fprintf(stdout, ",");
+            } else {
+                ring_comma = true;
+            }
+
+            fprintf(stdout, " [");
+            bool comma = false;
+            for ( auto pt = ring->begin(); pt != ring->end(); ++pt) {
+                if (comma) {
+                    fprintf(stdout, ",");
+                } else {
+                    comma = true;
+                }
+                fprintf(stdout, " [ %lld, %lld ]", pt->x, pt->y);
+            }
+            fprintf(stdout, " ]");
+        }
+        fprintf(stdout, " ] }");
+    }
+
+    void operator()(multi_polygon_type const & mpoly) const {
+        fprintf(stdout, "{ \"type\": \"MultiPolygon\", \"coordinates\": [");
+        bool poly_comma = false;
+        for ( auto poly = mpoly.begin(); poly != mpoly.end(); ++poly) {
+            if ( poly_comma ) {
+                fprintf(stdout, ",");
+            } else {
+                poly_comma = true;
+            }
+            fprintf(stdout, " [");
+            bool ring_comma = false;
+            for ( auto ring = poly->begin(); ring != poly->end(); ++ring) {
+                if (ring_comma) {
+                    fprintf(stdout, ",");
+                } else {
+                    ring_comma = true;
+                }
+                fprintf(stdout, " [");
+                bool comma = false;
+                for ( auto pt = ring->begin(); pt != ring->end(); ++pt) {
+                    if (comma) {
+                        fprintf(stdout, ",");
+                    } else {
+                        comma = true;
+                    }
+                    fprintf(stdout, " [ %lld, %lld ]", pt->x, pt->y);
+                }
+                fprintf(stdout, " ]");
+            }
+            fprintf(stdout, " ]");
+        }
+        fprintf(stdout, " ] }");
+    }
+
+    template <typename T>
+    void operator()(T const & g) const {
+        std::clog << "encountered unhandled shape";
+    }
+};
+
+std::vector<feature_type> output_within_tile(std::vector<feature_type> features, int z, unsigned x, unsigned y, bool &feature_comma) {
+    property_writer w;
+    shape_writer sw;
+    std::vector<feature_type> retval;
+    for ( feature_type feat : features ) {
+        // if within tile
+        if ( feat.properties[std::string("offtile")] == uint64_t(0) ) {
+            if ( feature_comma ) {
+                fprintf(stdout, ",\n");
+            } else {
+                feature_comma = true;
+            }
+            fprintf(stdout, "{ \"type\": \"Feature\",");
+            fprintf(stdout, "  \"properties\": {");
+            bool comma = false;
+            for ( auto it = feat.properties.begin(); it != feat.properties.end(); ++it ) {
+                if ( comma ) {
+                    fprintf(stdout, ",");
+                } else {
+                    comma = true;
+                }
+                fprintf(stdout, " \"%s\": ", it->first.c_str());
+                mapbox::util::apply_visitor(w, it->second);
+            }
+            fprintf(stdout, "},");
+            fprintf(stdout, " \"geometry\": ");
+            mapbox::util::apply_visitor(sw, feat.geometry);
+            fprintf(stdout, "}");
+        } else {
+            retval.push_back( feat );
+        }
+    }
+    return retval;
 }
--- readlayer.h
+++ readlayer.h
@@ -8,16 +8,20 @@
 #include "mvt.hpp"
 #include <mapbox/geometry/feature.hpp>
 
-using mapbox::geometry::feature_collection;
-using feature_collection_type = feature_collection<long long>;
 using mapbox::geometry::feature;
 using feature_type = feature<long long>;
 
 std::vector<feature_type> layer_to_features(mvt_layer const &layer, unsigned z, unsigned x, unsigned y);
+std::vector<feature_type> output_within_tile(std::vector<feature_type> features, int z, unsigned x, unsigned y, bool &feature_comma);
 
 const int NORTH = 0x01;
 const int SOUTH = 0x02;
 const int EAST = 0x04;
 const int WEST = 0x08;
+
+const int NO_INTERCECT = 0x01;
+const int COINCIDENT = 0x02;
+const int INTERCECT = 0x04;
+
 
 #endif //TIPPECANOE_READLAYER_H
